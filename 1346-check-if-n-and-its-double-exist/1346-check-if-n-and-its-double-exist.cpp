d9c82b7a3abb0ccb930c9f4b2517769f2a7a34b0
Suraj Mandal <122726373+SurajMandal14@users.noreply.github.com>
2024-12-06 14:41:37 +0530

    Time: 0 ms (100.00%), Space: 13.6 MB (71.02%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
     bool checkIfExist(vector<int>& arr) {
-        bitset<1001> seen[2]={0};
-        for(int x: arr){
-            int z=abs(x);
-            if ((z<=500 && seen[x<0][2*z])|| (z%2==0 && seen[x<0][z/2]))
-                return 1;
-            seen[x<0][z]=1;
-        }
-        return 0;
+        int j;
+       for(int i=0;i<arr.size();i++)
+       {
+           j=0;
+           while(j<arr.size()){
+           if(i!=j && arr[i]==2*arr[j])
+           {
+               return true;
+           }
+           else
+               j++;}
+       }
+        return false;
     }
 };
